ef7c6cbc88bd5b5677a887944c877afab642cb93
thogama <dethfiregrasp@gmail.com>
2023-04-17 12:03:53 -0300

    Entrada txt funcionando

--- main.cpp
+++ main.cpp
@@ -7,10 +7,37 @@
 #include <sstream>
 
 // gcc main.cpp -o main -lGL -lGLU -lglut -- para rodar
-std::ifstream arquivo("entrada.txt");
-std::string linha;
-void lerArquivo()
+
+float X;
+float Y;
+float Z;
+float ilha;
+float lagos;
+float terrestres_1;
+float terrestres_2;
+float plantas_1;
+float plantas_2;
+
+struct ValoresArquivo
 {
+    std::string X;
+    std::string Y;
+    std::string Z;
+    std::string ilha;
+    std::string lagos;
+    std::string terrestres_1;
+    std::string terrestres_2;
+    std::string plantas_1;
+    std::string plantas_2;
+};
+
+void lerArquivo(std::string nome_arquivo, float &saida_X, float &saida_Y, float &saida_Z,
+                float &saida_ilha, float &saida_lagos, float &saida_terrestes_1, float &saida_terrestes_2, float &saida_plantas_1, float &saida_plantas_2)
+{
+    ValoresArquivo valores;
+
+    std::ifstream arquivo(nome_arquivo);
+    std::string linha;
     std::string X, Y, Z, lixo, ilha, lagos, terrestres_1, terrestres_2, plantas_1, plantas_2;
 
     int cont = 0;
@@ -21,59 +48,67 @@ void lerArquivo()
         switch (cont)
         {
         case 0:
-            ss >> lixo >> X >> Y >> Z;
-            std::cout << "Valores de X, Y, Z: " << X << ", " << Y << ", " << Z << "\n";
+            ss >> lixo >> valores.X >> valores.Y >> valores.Z;
+            std::cout << "Valores de X, Y, Z: " << valores.X << ", " << valores.Y << ", " << valores.Z << "\n";
             break;
         case 1:
-            ss >> lixo >> ilha;
-            std::cout << "Valor de ilha: " << ilha << "\n";
+            ss >> lixo >> valores.ilha;
+            std::cout << "Valor de ilha: " << valores.ilha << "\n";
             break;
 
         case 2:
-            ss >> lixo >> lagos;
-            std::cout << "Valor de lagos: " << lagos << "\n";
+            ss >> lixo >> valores.lagos;
+            std::cout << "Valor de lagos: " << valores.lagos << "\n";
             break;
 
         case 3:
-            ss >> lixo >> terrestres_1;
-            std::cout << "Valor de terrestres_1: " << terrestres_1 << "\n";
+            ss >> lixo >> valores.terrestres_1;
+            std::cout << "Valor de terrestres_1: " << valores.terrestres_1 << "\n";
             break;
 
         case 4:
-            ss >> lixo >> terrestres_2;
-            std::cout << "Valor de terrestres_2: " << terrestres_2 << "\n";
+            ss >> lixo >> valores.terrestres_2;
+            std::cout << "Valor de terrestres_2: " << valores.terrestres_2 << "\n";
             break;
 
         case 5:
-            ss >> lixo >> plantas_1;
-            std::cout << "Valor de plantas_1: " << plantas_1 << "\n";
+            ss >> lixo >> valores.plantas_1;
+            std::cout << "Valor de plantas_1: " << valores.plantas_1 << "\n";
             break;
 
         case 6:
-            ss >> lixo >> plantas_2;
-            std::cout << "Valor de plantas_2: " << plantas_2 << "\n";
+            ss >> lixo >> valores.plantas_2;
+            std::cout << "Valor de plantas_2: " << valores.plantas_2 << "\n";
             break;
         }
         cont++;
     }
+    saida_X = std::stof(valores.X);
+    saida_Y = std::stof(valores.Y);
+    saida_Z = std::stof(valores.Z);
+    saida_ilha = std::stof(valores.ilha);
+    saida_lagos = std::stof(valores.lagos);
+    saida_terrestes_1 = std::stof(valores.terrestres_1);
+    saida_terrestes_2 = std::stof(valores.terrestres_2);
+    saida_plantas_1 = std::stof(valores.plantas_1);
+    saida_plantas_2 = std::stof(valores.plantas_2);
+
     arquivo.close();
 }
-
-float comprimento = 10;
-float largura = 10;
-float altura = 10;
-
-float basePontos[8][3] =
-    {
-        {0.0, 0.0, 0.0},                // a {0,0,0}
-        {comprimento, 0, 0.0},          // b {x,0,0}
-        {0.0, largura, 0.0},            // c {0,y,0}
-        {comprimento, largura, 0.0},    // d {x,y,0}
-        {0.0, 0.0, altura},             // e {0,0,z}
-        {comprimento, 0, altura},       // f {x,0,z}
-        {0.0, largura, altura},         // g {0,y,z}
-        {comprimento, largura, altura}, // h {x,y,z}
-};
+// float X = X;
+// float Y = Y;
+// float altura = Z;
+// float basePontos[8][3] =
+//     {
+//         {0.0, 0.0, 0.0}, // a {0,0,0}0
+//         {X, 0, 0.0},     // b {x,0,0}1
+//         {0.0, Y, 0.0},   // c {0,y,0}2
+//         {X, Y, 0.0},     // d {x,y,0}3
+//         {0.0, 0.0, Z},   // e {0,0,z}4
+//         {X, 0, Z},       // f {x,0,z}5
+//         {0.0, Y, Z},     // g {0,y,z}6
+//         {X, Y, Z},       // h {x,y,z}7
+// };
 
 void desenhaCubo(float x, float y, float z, float lado, GLfloat borda[3], GLfloat cor[3])
 {
@@ -145,45 +180,53 @@ void desenhaCubo(float x, float y, float z, float lado, GLfloat borda[3], GLfloa
     glEnd();
 };
 
-void base()
+void base(float X, float Y, float Z)
 {
+    GLfloat pos0[3] = {0.0, 0.0, 0.0}; // a {0,0,0}0
+    GLfloat pos1[3] = {X, 0, 0.0};     // b {x,0,0}1
+    GLfloat pos2[3] = {0.0, Y, 0.0};   // c {0,y,0}2
+    GLfloat pos3[3] = {X, Y, 0.0};     // d {x,y,0}3
+    GLfloat pos4[3] = {0.0, 0.0, Z};   // e {0,0,z}4
+    GLfloat pos5[3] = {X, 0, Z};       // f {x,0,z}5
+    GLfloat pos6[3] = {0.0, Y, Z};     // g {0,y,z}6
+    GLfloat pos7[3] = {X, Y, Z};       // h {x,y,z}7
     glBegin(GL_LINES);
     glColor3f(1.0, 1.0, 1.0);
-    glVertex3fv(basePontos[0]);
-    glVertex3fv(basePontos[1]);
+    glVertex3fv(pos0);
+    glVertex3fv(pos1);
 
-    glVertex3fv(basePontos[1]);
-    glVertex3fv(basePontos[3]);
+    glVertex3fv(pos1);
+    glVertex3fv(pos3);
 
-    glVertex3fv(basePontos[3]);
-    glVertex3fv(basePontos[2]);
+    glVertex3fv(pos3);
+    glVertex3fv(pos2);
 
-    glVertex3fv(basePontos[2]);
-    glVertex3fv(basePontos[0]);
+    glVertex3fv(pos2);
+    glVertex3fv(pos0);
 
-    glVertex3fv(basePontos[0]);
-    glVertex3fv(basePontos[4]);
+    glVertex3fv(pos0);
+    glVertex3fv(pos4);
 
-    glVertex3fv(basePontos[1]);
-    glVertex3fv(basePontos[5]);
+    glVertex3fv(pos1);
+    glVertex3fv(pos5);
 
-    glVertex3fv(basePontos[2]);
-    glVertex3fv(basePontos[6]);
+    glVertex3fv(pos2);
+    glVertex3fv(pos6);
 
-    glVertex3fv(basePontos[3]);
-    glVertex3fv(basePontos[7]);
+    glVertex3fv(pos3);
+    glVertex3fv(pos7);
 
-    glVertex3fv(basePontos[4]);
-    glVertex3fv(basePontos[5]);
+    glVertex3fv(pos4);
+    glVertex3fv(pos5);
 
-    glVertex3fv(basePontos[5]);
-    glVertex3fv(basePontos[7]);
+    glVertex3fv(pos5);
+    glVertex3fv(pos7);
 
-    glVertex3fv(basePontos[7]);
-    glVertex3fv(basePontos[6]);
+    glVertex3fv(pos7);
+    glVertex3fv(pos6);
 
-    glVertex3fv(basePontos[6]);
-    glVertex3fv(basePontos[4]);
+    glVertex3fv(pos6);
+    glVertex3fv(pos4);
 
     // Finaliza a definição das primitivas
     glEnd();
@@ -191,15 +234,15 @@ void base()
 
 float area()
 {
-    return comprimento * largura;
+    return X * Y;
 }
 
 float mover_agua = 0;
 void agua()
 {
-    for (float i = 0; i < comprimento; i += 0.5)
+    for (float i = 0; i < X; i += 0.5)
     {
-        for (float k = 0; k < largura; k += 0.5)
+        for (float k = 0; k < Y; k += 0.5)
         {
             GLfloat cor[3] = {31.0f / 255, 12.0f / 255, 243.0f / 255};
             GLfloat borda[3] = {1, 1, 1};
@@ -208,11 +251,11 @@ void agua()
     }
 }
 
-bool haCelulasLivres(bool **areaOcupada, int largura, int comprimento)
+bool haCelulasLivres(bool **areaOcupada, int Y, int X)
 {
-    for (int i = 0; i < largura; i++)
+    for (int i = 0; i < Y; i++)
     {
-        for (int j = 0; j < comprimento; j++)
+        for (int j = 0; j < X; j++)
         {
             if (!areaOcupada[i][j])
             {
@@ -240,7 +283,8 @@ void coqueiro(float x, float y, float z)
     GLfloat madeira[3] = {141.0f / 255, 90.0f / 255, 0.0f};
 
     float aux = 0.25;
-    while (aux < z)
+    float aleatorio = rand() % (int)z;
+    while (aux < aleatorio + 5)
     {
         desenhaCubo(x + 0.125, y + 0.125, aux, 0.25, borda, madeira);
         aux += 0.25;
@@ -289,25 +333,25 @@ void terra(float porcentagem, int coqueiros, int arbustos)
     int coqueirosCont = 0;
     int arbustosCont = 0;
 
-    srand((comprimento * largura * altura));
+    srand((X * Y * Z));
 
-    bool **areaOcupada = new bool *[(int)comprimento * 2];
-    for (int i = 0; i < comprimento * 2; i++)
+    bool **areaOcupada = new bool *[(int)X * 2];
+    for (int i = 0; i < X * 2; i++)
     {
-        areaOcupada[i] = new bool[(int)largura * 2];
-        for (int j = 0; j < largura * 2; j++)
+        areaOcupada[i] = new bool[(int)Y * 2];
+        for (int j = 0; j < Y * 2; j++)
         {
             areaOcupada[i][j] = false;
         }
     }
 
-    for (float i = 0; i < numCubos && haCelulasLivres(areaOcupada, comprimento, largura); i += 0.5)
+    for (float i = 0; i < numCubos && haCelulasLivres(areaOcupada, X, Y); i += 0.5)
     {
         int aleatorioX, aleatorioY;
         do
         {
-            aleatorioX = rand() % (int)comprimento;
-            aleatorioY = rand() % (int)largura;
+            aleatorioX = rand() % (int)X;
+            aleatorioY = rand() % (int)Y;
         } while (areaOcupada[aleatorioX][aleatorioY]);
 
         areaOcupada[aleatorioX][aleatorioY] = true;
@@ -334,18 +378,18 @@ void terra(float porcentagem, int coqueiros, int arbustos)
             switch (aux)
             {
             case 0:
-                coqueiro(centroX + 0.5, centroY, alturaAtual + altura / 2);
+                coqueiro(centroX + 0.5, centroY, alturaAtual + Z / 2);
                 break;
             case 1:
-                coqueiro(centroX + 0.5, centroY + 0.5, alturaAtual + altura / 2);
+                coqueiro(centroX + 0.5, centroY + 0.5, alturaAtual + Z / 2);
 
                 break;
             case 2:
-                coqueiro(centroX, centroY + 0.5, alturaAtual + altura / 2);
+                coqueiro(centroX, centroY + 0.5, alturaAtual + Z / 2);
 
                 break;
             default:
-                coqueiro(centroX, centroY, alturaAtual + altura / 2);
+                coqueiro(centroX, centroY, alturaAtual + Z / 2);
                 break;
             }
             coqueirosCont++;
@@ -371,7 +415,7 @@ void terra(float porcentagem, int coqueiros, int arbustos)
         }
     }
 
-    for (int i = 0; i < comprimento; i++)
+    for (int i = 0; i < X; i++)
     {
         delete[] areaOcupada[i];
     }
@@ -450,16 +494,16 @@ void display()
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    double cameraTargetX = comprimento / 2 + sin(cameraAngleY) * cos(cameraAngleX);
-    double cameraTargetY = largura / 2 + sin(cameraAngleX);
-    double cameraTargetZ = altura / 2 - cos(cameraAngleY) * cos(cameraAngleX);
+    double cameraTargetX = X / 2 + sin(cameraAngleY) * cos(cameraAngleX);
+    double cameraTargetY = Y / 2 + sin(cameraAngleX);
+    double cameraTargetZ = Z / 2 - cos(cameraAngleY) * cos(cameraAngleX);
     gluLookAt(cameraX, cameraY, cameraZ, cameraTargetX, cameraTargetY + rotate, cameraTargetZ, 0, 0, 1);
     drawAxes(5.0f);
 
     glRotatef(6 * rotateKey, 0, 0, 1);
-    base();
+    base(X, Y, Z);
     agua();
-    terra(50, 5, 5); // colocar entradas ainda
+    terra(ilha, plantas_1, plantas_2); // colocar entradas ainda
     glutSwapBuffers();
 }
 
@@ -473,7 +517,7 @@ void idleFunc()
 }
 int main(int argc, char **argv)
 {
-    lerArquivo();
+    lerArquivo("entrada.txt", X, Y, Z, ilha, lagos, terrestres_1, terrestres_2, plantas_1, plantas_2);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
     glutInitWindowSize(1280, 720);
